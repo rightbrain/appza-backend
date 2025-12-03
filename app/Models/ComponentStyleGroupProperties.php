a3ec7bf233f7ef67a7c6733a7830dbcde6baa9c1
rbraju3m <rbraju3m@gmail.com>
2025-12-03 12:27:10 +0600

    component import

--- app/Models/ComponentStyleGroupProperties.php
+++ app/Models/ComponentStyleGroupProperties.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ComponentStyleGroupProperties extends Model
 {
-    use HasFactory;
 
     protected $table = 'appfiy_component_style_group_properties';
     public $timestamps = true;
@@ -15,4 +13,8 @@ class ComponentStyleGroupProperties extends Model
     protected $dates = ['deleted_at','created_at','updated_at'];
     protected $fillable = ['component_id', 'name', 'input_type', 'value','style_group_id'];
 
+    public function styleGroup()
+    {
+        return $this->belongsTo(StyleGroup::class, 'style_group_id', 'id');
+    }
 }
